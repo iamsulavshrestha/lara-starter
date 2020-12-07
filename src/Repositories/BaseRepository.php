27ed53bdd033d8857bb9293ccdfc9d02bb001322
Sulav Shrestha <connect@sulav.name.np>
2020-12-07 16:57:47 +0545

    add base contracts and repositories

---
+++ src/Repositories/BaseRepository.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Sushre\Starter\Repositories;
+
+use Sushre\Starter\Contracts\BaseContract;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseContract{
+    protected $model;
+
+    public function __construct(Model $model){
+      $this->model = $model;
+    }
+
+    /**
+     * @param array $columns
+     * @param string $orderBy
+     * @param string $sortBy
+     * @return mixed
+     */
+    public function all(string $orderBy = 'created_at', string $sortBy = 'desc', $columns = array('*'))
+    {
+        return $this->model->orderBy($orderBy, $sortBy)->get($columns);
+    }
+
+    /**
+     * @param array $attributes
+     * @return mixed
+     */
+    public function create(array $attributes)
+    {
+        return $this->model->create($attributes);
+    }
+
+    /**
+     * @param array $attributes
+     * @param int $id
+     * @return bool
+     */
+    public function update(array $attributes, int $id) : bool
+    {
+        return $this->find($id)->update($attributes);
+    }
+
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function delete(int $id) : bool
+    {
+        return $this->model->find($id)->delete();
+    }
+    
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function find(int $id)
+    {
+        return $this->model->find($id);
+    }
+
+    /**
+     * @param int $id
+     * @return mixed
+     * @throws ModelNotFoundException
+     */
+    public function findOneOrFail(int $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function findBy(array $data)
+    {
+        return $this->model->where($data)->get();
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     */
+    public function findOneBy(array $data)
+    {
+        return $this->model->where($data)->first();
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws ModelNotFoundException
+     */
+    public function findOneByOrFail(array $data)
+    {
+        return $this->model->where($data)->firstOrFail();
+    }
+
+    public function paginate($perPage = 10, $columns = ['*']){
+    return $this->model->paginate($perPage, $columns);
+  }
+
+
+}
